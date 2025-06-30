7fc30e01caece399b61581c1c92f3e0e3b4b9e13
hosted-fornet <hosted-fornet@protonmail.com>
2025-06-30 09:11:27 -0700

    fix typo

--- hyperware_app_common/src/lib.rs
+++ hyperware_app_common/src/lib.rs
@@ -187,7 +187,7 @@ pub enum AppSendError {
 }
 
 pub async fn sleep(sleep_ms: u64) -> Result<(), AppSendError> {
-    let timer_request = Request::to(("our", "timer", "distro", "sys"))
+    let request = Request::to(("our", "timer", "distro", "sys"))
         .body(timer::TimerAction::SetTimer(sleep_ms))
         .expects_response((sleep_ms / 1_000) + 1);
 
